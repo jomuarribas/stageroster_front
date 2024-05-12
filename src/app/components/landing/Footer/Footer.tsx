b1fcf475e4b48c27e5ace5b65913ab0ce00a5a69
jmarribas <jomuarribas@gmail.com>
2024-05-12 18:42:17 +0200

    contact

--- src/app/components/landing/Footer/Footer.tsx
+++ src/app/components/landing/Footer/Footer.tsx
@@ -10,11 +10,11 @@ export default function Footer() {
         width="30"
         height="30"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth={1.5}
         stroke="#2b232a"
         fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
--- src/app/components/landing/FormSection/FormSection.tsx
+++ src/app/components/landing/FormSection/FormSection.tsx
@@ -3,17 +3,34 @@
 import ReCAPTCHA from 'react-google-recaptcha';
 import styles from './FormSection.module.css';
 import { useRef } from 'react';
+import { useApi } from '@/app/hooks/useApi';
 
 export default function FormSection() {
   const recaptcha = useRef();
+  const formRef = useRef(null);
+  const { apiFetch } = useApi();
 
-  const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const captchaValue = recaptcha.current.getValue();
     if (!captchaValue) {
       return;
     }
-    const form = e.currentTarget;
+    const route = `users/contact`;
+    const form = {
+      name: e.currentTarget[0].value,
+      email: e.currentTarget[1].value,
+      message: e.currentTarget[2].value,
+    };
+
+    try {
+      const fetch = await apiFetch(true, 'POST', route, form, null);
+      if (fetch.message) {
+        formRef.current.reset();
+      }
+    } catch (error) {
+      return console.log(error);
+    }
   };
 
   return (
@@ -25,7 +42,7 @@ export default function FormSection() {
           contacto con nosotros, rellena el siguiente formulario y te
           contestaremos lo antes posible.
         </p>
-        <form onSubmit={handleForm}>
+        <form ref={formRef} onSubmit={handleForm}>
           <label htmlFor="name">Nombre:</label>
           <input type="text" id="name" name="name" required />
           <label htmlFor="email">Email:</label>
